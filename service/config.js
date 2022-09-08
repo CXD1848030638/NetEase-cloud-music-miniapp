5af30dca5509e3f2cd33189f2331b6eb7f0b29ca
kunliu11 <kunliu11@iflytek.com>
2022-09-08 09:49:30 +0800

    提交最终版

---
+++ service/config.js
@@ -0,0 +1,6 @@
+// export const baseURL = 'http://localhost:3000'
+export const baseURL = 'https://mu-api.yuk0.com'
+
+// export const baseURL = 'https://lk-music.cn1.utools.club'
+
+export const timeout = 50000
