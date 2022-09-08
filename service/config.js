c94bf66faa85c235c26b09f5252052573c352ccb
kunliu11 <kunliu11@iflytek.com>
2022-09-08 09:57:53 +0800

    删除个人信息，更新readme文档

--- service/config.js
+++ service/config.js
@@ -1,6 +1,3 @@
-// export const baseURL = 'http://localhost:3000'
-export const baseURL = 'https://mu-api.yuk0.com'
-
-// export const baseURL = 'https://lk-music.cn1.utools.club'
+export const baseURL = 'http://localhost:3000'
 
 export const timeout = 50000
